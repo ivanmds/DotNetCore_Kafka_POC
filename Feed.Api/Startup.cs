70d70323f3241439ab4d701b17673c1f9f6c5540
Ivan Meireles da Silva <isilva@meuacesso.com.br>
2019-10-04 16:27:21 -0300

    ajustes

--- Feed.Api/Startup.cs
+++ Feed.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Confluent.Kafka;
+using Feed.Api.Workers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,12 @@ namespace Feed.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //services.AddHostedService<ReadTopicConsumer>();
+            //var consumerConfig = new ConsumerConfig { GroupId = "test-consumer-group", BootstrapServers = "broker:29092" };
+            //Configuration.Bind("topicfeed2", consumerConfig);
+
+            //services.AddSingleton(consumerConfig);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Feed.Api/Workers/ReadTopicConsumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+namespace Feed.Api.Workers
+{
+    public class ReadTopicConsumer : BackgroundService
+    {
+        private readonly ConsumerConfig _consumerConfig;
+
+        public ReadTopicConsumer(ConsumerConfig consumerConfig)
+        {
+            _consumerConfig = consumerConfig;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            Console.WriteLine("OrderProcessing Service Started");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var consumerHelper = new ConsumerWrapper(_consumerConfig, "topicfeed2");
+                string orderRequest = consumerHelper.ReadMessage();
+
+                //TODO:: Process Order
+                Console.WriteLine($"Info: OrderHandler => Processing the order for {orderRequest}");
+            }
+        }
+    }
+}
