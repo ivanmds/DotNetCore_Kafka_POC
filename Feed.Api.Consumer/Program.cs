cfa7b0f5dd02dd5889f243c644d9048d2da5574d
Ivan Meireles da Silva <isilva@meuacesso.com.br>
2019-10-08 14:10:09 -0300

    ajuste

---
+++ Feed.Api.Consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Feed.Api.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndpoint = "localhost:9092";
+
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = kafkaEndpoint,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("topicfeed2");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel(); 
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            throw e;
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
